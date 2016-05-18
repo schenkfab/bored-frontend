25a0c60c38002c412dfe07ee0287c40bc80bc5bc
schenkfab <schenkfab@users.noreply.github.com>
2016-05-18 19:06:02 +0200

    put login controller in separate file

---
+++ src/app/controllers/login.js
@@ -0,0 +1,9 @@
+/* eslint no-param-reassign: ["error", { "props": false }]*/
+
+angular.module('myApp').controller('loginCtrl', ($scope) => {
+  $scope.user = {};
+
+  $scope.user.login = () => {
+    console.log($scope.user);
+  };
+});
--- src/app/controllers/main.js
+++ src/app/controllers/main.js
@@ -3,11 +3,3 @@
 angular.module('myApp').controller('mainCtrl', () => {
 
 });
-
-angular.module('myApp').controller('loginCtrl', ($scope) => {
-  $scope.user = {};
-
-  $scope.user.login = () => {
-    console.log($scope.user);
-  };
-});
