e65b3ca7c61d365dc8a486ed1d77fb224ac328ef
schenkfab <schenkfab@users.noreply.github.com>
2016-05-18 17:29:42 +0200

    feat: add login directive

---
+++ src/app/directives/login.js
@@ -0,0 +1,7 @@
+/* eslint arrow-body-style: [""] */
+angular.module('myApp').directive('login', () => {
+  return {
+    templateUrl: 'app/templates/login.html', // Where is the html code for the directive?
+    replace: true, // Do you wan't to display the directive tag?
+  };
+});
