9cd077086caab7be58de3108be0fdd7835408084
schenkfab <schenkfab@users.noreply.github.com>
2016-05-18 17:30:28 +0200

    feat: add angular login controller

--- src/app/controllers/main.js
+++ src/app/controllers/main.js
@@ -1,3 +1,13 @@
-angular.module('myApp').controller('mainCtrl', function(pageService, $scope, $interval) {
-	
+/* eslint no-param-reassign: ["error", { "props": false }]*/
+
+angular.module('myApp').controller('mainCtrl', () => {
+
+});
+
+angular.module('myApp').controller('loginCtrl', ($scope) => {
+  $scope.user = {};
+
+  $scope.user.login = () => {
+    console.log($scope.user);
+  };
 });
