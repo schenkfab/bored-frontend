154a12c4b550d02fa891001330702376b6d73c33
schenkfab <schenkfab@users.noreply.github.com>
2016-05-29 13:12:36 +0200

    feat: service to send a message

--- src/app/services/message.js
+++ src/app/services/message.js
@@ -1,6 +1,23 @@
-angular.module('myApp').service('messageService', function ($http, authenticationService, configService) {
+angular.module('myApp').service('messageService', function ($http, $q, authenticationService, configService) {
 
   this.messages = [];
+
+  this.sendMessage = (message) => {
+    return $q((resolve, reject) => {
+      $http.post(configService.REST_URLS.messages, {
+        message: message.text,
+        token: authenticationService.token,
+        receiver: message.receiver,
+      })
+      .then(
+        (response) => {
+          resolve(response);
+        },
+        (error) => {
+          reject(error);
+        });
+    });
+  };
 
   this.getMessages = () => {
     $http.get(configService.REST_URLS.messages, {
