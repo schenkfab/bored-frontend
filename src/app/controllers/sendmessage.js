298146060ef5b30b6e3db59911cf235761c4b827
schenkfab <schenkfab@users.noreply.github.com>
2016-06-03 18:51:54 +0200

    display a message after it was sent

--- src/app/controllers/sendmessage.js
+++ src/app/controllers/sendmessage.js
@@ -1,13 +1,27 @@
 /* eslint no-param-reassign: ["error", { "props": false }]*/
 
 angular.module('myApp')
-.controller('sendMessageCtrl', ($scope, authenticationService, pageService, messageService) => {
+.controller('sendMessageCtrl', function($scope, authenticationService, pageService, messageService) {
   $scope.auth = authenticationService;
   $scope.page = pageService;
 
   $scope.data = {
     receiver: { name: 'Hans', _id: '' },
     messages: [],
+    send: () => {
+      messageService.sendMessage({
+        text: $scope.data.reply,
+        receiver: $scope.data.receiver._id,
+      })
+      .then((response) => {
+        console.log(response);
+        messageService.getMessagesByUser($scope.data.receiver._id)
+        .then((response) => {
+          console.log(response);
+          $scope.data.messages = response.data;
+        });
+      });
+    },
   };
 
   $scope.$watch('page.page', (newValue, oldValue) => {
--- src/app/directives/sendmessage.js
+++ src/app/directives/sendmessage.js
@@ -2,7 +2,6 @@
 angular.module('myApp').directive('sendmessage', () => {
   return {
     templateUrl: 'app/templates/sendmessage.html', // Where is the html code for the directive?
-    controller: 'sendMessageCtrl',
     replace: true, // Do you wan't to display the directive tag?
   };
 });
